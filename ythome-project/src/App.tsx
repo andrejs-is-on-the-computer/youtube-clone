0bc80daebf9cbd4ace63c247fa3bfa222d59f791
andrejs-is-on-the-computer <63778410+andrejs-is-on-the-computer@users.noreply.github.com>
2024-01-10 19:43:27 +0100

    categories completed

--- ythome-project/src/App.tsx
+++ ythome-project/src/App.tsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { CategoryPills } from "./components/CategoryPills";
+import { categories } from "./data/home";
 import { PageHeader } from "./layouts/PageHeader";
 
 export default function App() {
+  const [selectedCategory, setSelectedCategory] = useState(categories[0])
   return <div className="max-h-screen flex flex-col">
       <PageHeader />
       <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
@@ -10,9 +13,14 @@ export default function App() {
           Sidebar
         </div>
 
-        <div className="sticky top-0 z-10 pb-4">
-          <CategoryPills categories={categories} />
+        <div className="overflow-x-hidden px-8 pb-4">
+          <div className="sticky top-0 z-10 pb-4">
+            <CategoryPills categories={categories} 
+              selectedCategory={selectedCategory} 
+              onSelect={setSelectedCategory} />
+          </div>
         </div>
+        
 
       </div>
   </div>
