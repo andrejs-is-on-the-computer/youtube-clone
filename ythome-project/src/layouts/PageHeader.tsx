8aad456a71e82a8c69730b20d8dca21b907fea12
andrejs-is-on-the-computer <63778410+andrejs-is-on-the-computer@users.noreply.github.com>
2024-01-10 17:11:45 +0100

    custom button

--- ythome-project/src/layouts/PageHeader.tsx
+++ ythome-project/src/layouts/PageHeader.tsx
@@ -1,13 +1,14 @@
 import { Menu } from 'lucide-react';
 import logo from '../assets/metoobe.png'
+import { Button } from '../components/Button';
 
 export function PageHeader() {
     return <div className="flex gap-10 lg:gap-20 justify-between">
         {/* Logo and Hamburger Button */}
         <div className="flex gap-4 items-center flex-shrink-0">
-            <button>
+            <Button variant="ghost" size="icon" className='m-10'>
                 <Menu />
-            </button>
+            </Button>
             <a href="/">
                 <img src={logo} className='h-8' alt="metoob logo" />
             </a>
