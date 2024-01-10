ba19511743bef0be2211a1454e9c157d80bd5e65
andrejs-is-on-the-computer <63778410+andrejs-is-on-the-computer@users.noreply.github.com>
2024-01-10 16:21:51 +0100

    button component, tailwind extension

---
+++ ythome-project/src/components/Button.tsx
@@ -0,0 +1,16 @@
+import { cva } from "class-variance-authority"
+
+const buttonStyles = cva(["hover:bg-secondary-hover", "transition-colors"], {
+    variants: {
+        size: {
+            default: [],
+            icon: ["rounded-full", "w-10", "h-10", "flex", "items-center", "justify-center", "p-2.5"]
+        }
+    }
+})
+
+const classes = buttonStyles({ size: "icon "})
+
+export function Button() {
+    return <button />
+}
